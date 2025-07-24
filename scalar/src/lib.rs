f41d3244cf357255fd722d36f810b59c362e8ab1
aelidris <smias1.1998@gmail.com>
2025-07-24 11:06:42 +0100

    scalar

---
+++ scalar/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn sum(a: u8, b: u8) -> u8 {
+    a.checked_add(b).expect("ERROR: attempt to add with overflow")
+}
+
+pub fn diff(a: i16, b: i16) -> i16 {
+    a.checked_sub(b).expect("ERROR: attempt to subtract with overflow")
+}
+
+pub fn pro(a: i8, b: i8) -> i8 {
+    a.checked_mul(b).expect("ERROR: attempt to multiply with overflow")
+}
+
+pub fn quo(a: f32, b: f32) -> f32 {
+    a/b
+}
+
+pub fn rem(a: f32, b: f32) -> f32 {
+    a%b
+}
