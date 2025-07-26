519ad3d8faab599d29c3e7b798b855783824994e
aelidris <smias1.1998@gmail.com>
2025-07-26 17:26:40 +0100

    arrange_it

--- arrange_it/src/lib.rs
+++ arrange_it/src/lib.rs
@@ -1,26 +1,21 @@
 pub fn arrange_phrase(phrase: &str) -> String {
-    let mut words_with_pos: Vec<(u32, String)> = phrase
-        .split_whitespace()
-        .map(|word| {
-            let pos = word
-                .chars()
-                .find(|c| c.is_ascii_digit())
-                .unwrap()
-                .to_digit(10)
-                .unwrap();
-            let cleaned = word
+    let mut words: Vec<&str> = phrase.split_whitespace().collect();
+
+    words.sort_by_key(|word| {
+        word.chars()
+            .find(|c| c.is_ascii_digit())
+            .and_then(|c| c.to_digit(10))
+            .unwrap_or(0)
+    });
+
+    words
+        .iter()
+        .map(|word|
+            word
                 .chars()
                 .filter(|c| !c.is_ascii_digit())
-                .collect();
-            (pos, cleaned)
-        })
-        .collect();
-
-    words_with_pos.sort_by_key(|&(pos, _)| pos);
-
-    words_with_pos
-        .into_iter()
-        .map(|(_, word)| word)
+                .collect::<String>()
+        )
         .collect::<Vec<_>>()
         .join(" ")
 }
