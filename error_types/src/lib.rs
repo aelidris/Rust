e6b8e53748f8a057ed7713306009ed6cd50702fb
aelidris <smias1.1998@gmail.com>
2025-08-01 13:53:21 +0100

    error_types

---
+++ error_types/src/lib.rs
@@ -0,0 +1,55 @@
+use chrono::Local;
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct FormError {
+    pub form_values: (&'static str, String),
+    pub date: String,
+    pub err: &'static str,
+}
+
+impl FormError {
+    pub fn new(field_name: &'static str, field_value: String, err: &'static str) -> Self {
+        let date = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
+        FormError {
+            form_values: (field_name, field_value),
+            date,
+            err,
+        }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Form {
+    pub name: String,
+    pub password: String,
+}
+
+impl Form {
+    pub fn validate(&self) -> Result<(), FormError> {
+        if self.name.is_empty() {
+            return Err(FormError::new("name", self.name.clone(), "Username is empty"));
+        }
+
+        if self.password.len() < 8 {
+            return Err(FormError::new(
+                "password",
+                self.password.clone(),
+                "Password should be at least 8 characters long",
+            ));
+        }
+
+        let has_alpha = self.password.chars().any(|c| c.is_ascii_alphabetic());
+        let has_digit = self.password.chars().any(|c| c.is_ascii_digit());
+        let has_symbol = self.password.chars().any(|c| c.is_ascii_punctuation());
+
+        if !(has_alpha && has_digit && has_symbol) {
+            return Err(FormError::new(
+                "password",
+                self.password.clone(),
+                "Password should be a combination of ASCII numbers, letters and symbols",
+            ));
+        }
+
+        Ok(())
+    }
+}
