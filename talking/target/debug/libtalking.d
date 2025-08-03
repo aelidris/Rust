084ab974adeb7772d46bf5664b55dc4b4190d8de
aelidris <smias1.1998@gmail.com>
2025-08-03 14:27:37 +0100

    talking

--- talking/target/debug/libtalking.d
+++
@@ -1,1 +0,0 @@
-/home/aelidris/Desktop/piscine-rust/talking/target/debug/libtalking.rlib: /home/aelidris/Desktop/piscine-rust/talking/src/lib.rs
--- talking/target/debug/talking.d
+++
@@ -1,1 +0,0 @@
-/home/aelidris/Desktop/piscine-rust/talking/target/debug/talking: /home/aelidris/Desktop/piscine-rust/talking/src/lib.rs /home/aelidris/Desktop/piscine-rust/talking/src/main.rs
