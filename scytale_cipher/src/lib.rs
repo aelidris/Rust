b35b8de3e23e9fd2d5f33630306b3ff3febad711
aelidris <smias1.1998@gmail.com>
2025-08-04 18:25:42 +0100

    scytale_cipher

--- scytale_cipher/src/lib.rs
+++ scytale_cipher/src/lib.rs
@@ -1,10 +1,23 @@
+use std::iter;
+
 pub fn scytale_cipher(message: &str, i: usize) -> String {
-    let mut res = String::new();
-    let mut target = 0;
-    let mut initial = 0;
-    while res.len() < message.len() {
-        res.push(message.chars().nth(target).unwrap());
-        if (target + i) < message.len() {target += i;} else {initial+=1; target = initial;} 
+    let i = i.min(message.len());
+    if message.is_empty() || i == 0 {
+        String::new()
+    } else {
+        let cols = message.len().div_ceil(i);
+
+        (0..i)
+            .map(|n| {
+                message
+                    .chars()
+                    .skip(n)
+                    .step_by(i)
+                    .chain(iter::repeat_n(' ', cols - (message.len() - n).div_ceil(i)))
+                    .collect::<String>()
+            })
+            .collect::<String>()
+            .trim_end()
+            .to_owned()
     }
-    res
 }
