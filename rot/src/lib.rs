b13fb5113bba7bf5fdc2f9ce821a94e37f58c5b4
aelidris <smias1.1998@gmail.com>
2025-08-04 17:06:16 +0100

    rot

--- rot/src/lib.rs
+++ rot/src/lib.rs
@@ -1,39 +1,12 @@
 pub fn rotate(input: &str, key: i8) -> String {
-    let mut ro = key;
-    if key < 0 {
-        ro = (25+key) % 26;
-    }
-    let all = "abcdefghijklmnopqrstuvwxyz".to_string();
-    let all_v2 = all.to_uppercase();
-    let mut res = String::new();
-    let mut new_index;
-    for chaaar in input.chars() {
-        new_index = (helper(chaaar) + (ro as usize)) % 26;
-        if chaaar.is_ascii_lowercase() {
-            res.push(all.chars().nth(new_index).unwrap());
-        } else if chaaar.is_ascii_uppercase() {
-            res.push(all_v2.chars().nth(new_index).unwrap());
+    input.chars().map(|c| {
+        if c.is_ascii_alphabetic() {
+            let base = if c.is_ascii_lowercase() { b'a' } else { b'A' } as i16;
+            let c_val = c as i16 - base;
+            let rotated = (c_val + key as i16).rem_euclid(26) + base;
+            rotated as u8 as char
         } else {
-            res.push(chaaar);
+            c
         }
-    }
-    res
+    }).collect()
 }
-
-pub fn helper(ch: char) -> usize {
-    let mut all = String::new();
-    if ch.is_lowercase() {
-        all = "abcdefghijklmnopqrstuvwxyz".to_string();
-    } else if ch.is_uppercase() {
-        all = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".to_string();
-    }
-    let mut res = 0;
-    for (i, c) in all.chars().enumerate() {
-        if c == ch {
-            res = i;
-            break;
-        }
-    }
-    return res;
-}
-
