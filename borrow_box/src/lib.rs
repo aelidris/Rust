07c2dc74572e0eefead36183730bdc6a5b910c6c
aelidris <smias1.1998@gmail.com>
2025-08-09 11:04:05 +0100

    borrow_box

---
+++ borrow_box/src/lib.rs
@@ -0,0 +1,44 @@
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct GameSession {
+    pub id: u32,
+    pub p1: (String, u32),
+    pub p2: (String, u32),
+    pub nb_games: u32,
+}
+
+impl GameSession {
+    pub fn new(id: u32, p1_name: String, p2_name: String, nb_games: u32) -> GameSession {
+        GameSession {
+            id,
+            p1: (p1_name, 0),
+            p2: (p2_name, 0),
+            nb_games,
+        }
+    }
+
+    pub fn read_winner(&self) -> Option<&(String, u32)> {
+        let required_wins = self.nb_games / 2 + 1;
+
+        match (self.p1.1 >= required_wins, self.p2.1 >= required_wins) {
+            (true, false) => Some(&self.p1),
+            (false, true) => Some(&self.p2),
+            _ => None,
+        }
+    }
+
+    pub fn update_score(&mut self, user_name: &str) {
+        if self.read_winner().is_some() {
+            return;
+        }
+
+        if user_name == self.p1.0 {
+            self.p1.1 += 1;
+        } else if user_name == self.p2.0 {
+            self.p2.1 += 1;
+        }
+    }
+
+    pub fn delete(self) -> String {
+        format!("game deleted: id -> {}", self.id)
+    }
+}
