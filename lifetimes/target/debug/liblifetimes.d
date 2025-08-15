34f098f3df6a857b03c0575772400682e0652fd6
aelidris <smias1.1998@gmail.com>
2025-08-15 14:41:38 +0100

    lifetime

--- lifetimes/target/debug/liblifetimes.d
+++
@@ -1,1 +0,0 @@
-/home/aelidris/Desktop/piscine-rust/lifetimes/target/debug/liblifetimes.rlib: /home/aelidris/Desktop/piscine-rust/lifetimes/src/lib.rs
--- lifetimes/target/debug/lifetimes.d
+++
@@ -1,1 +0,0 @@
-/home/aelidris/Desktop/piscine-rust/lifetimes/target/debug/lifetimes: /home/aelidris/Desktop/piscine-rust/lifetimes/src/lib.rs /home/aelidris/Desktop/piscine-rust/lifetimes/src/main.rs
