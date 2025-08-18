ab809d3204181b5192bafd718d677b0afc08871a
aelidris <smias1.1998@gmail.com>
2025-08-18 20:15:46 +0100

    get products

---
+++ get_products/src/lib.rs
@@ -0,0 +1,24 @@
+pub fn get_products(arr: Vec<usize>) -> Vec<usize> {
+    let n = arr.len();
+    if n == 0 {
+        return vec![];
+    }
+
+    let mut prefix = vec![1; n];
+    let mut suffix = vec![1; n];
+    let mut result = vec![1; n];
+
+    for i in 1..n {
+        prefix[i] = prefix[i - 1] * arr[i - 1];
+    }
+
+    for i in (0..n - 1).rev() {
+        suffix[i] = suffix[i + 1] * arr[i + 1];
+    }
+
+    for i in 0..n {
+        result[i] = prefix[i] * suffix[i];
+    }
+
+    result
+}
